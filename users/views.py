a25950fe707ba9458bd9656397e0e77da80be649
Gokhan <gknsvsdata@gmail.com>
2022-09-01 21:36:55 +0300

    logout

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponse
-
 from django.contrib import messages
+from django.contrib.auth import logout
 # Create your views here.
 
 def home(request):
     return render(request, 'users/home.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
